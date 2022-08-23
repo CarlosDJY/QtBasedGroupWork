311413fe7bd8f025ff8507a29883b2dd692266ff
CarlosDJY <103446832+CarlosDJY@users.noreply.github.com>
2022-08-23 16:54:32 +0800

    Add files via upload

---
+++ mainCode/login.h
@@ -0,0 +1,35 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <QMessageBox>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+
+private slots:
+    void on_BackButton_clicked();
+    void on_ConfirmButton_clicked();
+
+    void on_AccountLineEdit_editingFinished();
+
+    void on_PasswordLineEdit_editingFinished();
+
+    void on_AccountLineEdit_textChanged(const QString &arg1);
+
+private:
+    Ui::Login *ui;
+};
+
+
+#endif // LOGIN_H
