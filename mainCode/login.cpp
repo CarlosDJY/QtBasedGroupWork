07f8ee1ce0aecae79099a368980e374cd589d785
YaoanYang <yaoan.yang@se21.qmul.ac.uk>
2022-08-24 19:27:38 +0800

    first commit by yaoan

--- mainCode/login.cpp
+++ mainCode/login.cpp
@@ -4,8 +4,9 @@
 #include "ui_login.h"
 #include <QDebug>
 #include <QMessageBox>
-
-
+#include <stdio.h>
+#include <QFile>
+#include <qstring.h>
 Login::Login(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Login)
@@ -27,12 +28,44 @@ void Login::on_BackButton_clicked()
     this->close();
 }
 
-int isExistingAccount(QString){
-    return 1;
+int isExistingAccount(QString Q){
+    QFile File1("Users.txt");
+    File1.open(QIODevice::ReadOnly);
+    QString Arr;
+    int i=0;
+    while(!File1.atEnd())
+       {
+         Arr=(QString)File1.readLine();
+         QStringList P=Arr.split(' ');
+         if(QString::compare(P[0],Q)==0)
+            {
+                    File1.close();
+                    return true;
+            }
+       }
+    File1.close();
+    return false;
 }
 
-int isMappingPassword(QString, QString){
-    return 1;
+int isMappingPassword(QString Account, QString Password){
+    QFile File1("Users.txt");
+    File1.open(QIODevice::ReadOnly);
+    QString Arr;
+    int i=0;
+    while(!File1.atEnd())
+    {
+         Arr=(QString)File1.readLine();
+         QStringList P=Arr.split(' ');
+         if(isExistingAccount(Account))
+         {
+             qDebug()<<"rua";
+             if(QString::compare(P[1],Password)==0)
+             {
+                 return true;
+             }
+         }
+    }
+    return false;
 }
 
 void Login::on_ConfirmButton_clicked()
