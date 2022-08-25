5dc622d6507a251966fd1448d7ae0f9470ce64f4
Yaoan Yang <yaoan.yang@se21.qmul.ac.uk>
2022-08-25 12:58:07 +0800

    Clean all qDebug() in the register.cpp

--- mainCode/productsearch.h
+++ mainCode/productsearch.h
@@ -5,7 +5,20 @@
 #include <QProcess>
 #include <QMessageBox>
 #include <QButtonGroup>
-
+#include <time.h>
+struct Good
+{
+    QString Name;
+    float SellPrice;
+    float BuyPrice;
+    float Discount;//此变量表示折扣，如为八折，该变量设置为0.8，其他折扣同理
+    QString Shop;
+    QString ID;
+    int Storage;
+    time_t EndTime;
+    time_t StartTime;
+};
+typedef struct Good Good;
 namespace Ui {
 class ProductSearch;
 }
